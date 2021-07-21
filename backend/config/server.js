3706553b8e0bed9e9b5c8c2a0cdbc7393c090813
Dmitry Balaev <dabalaev88@gmail.com>
2021-07-21 17:34:31 +0300

    back

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8019711d0195e20ef588de27e26e534'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a99587a-dbed-458f-9cfd-c7926bbd9eab'
+};
